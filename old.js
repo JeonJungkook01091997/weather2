f836dcedc01f285e40041e2bbe2353081f66cebb
Jyotsana <111308196+JeonJungkook01091997@users.noreply.github.com>
2024-09-17 01:00:55 +0530

    Add files via upload

---
+++ old.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/weather/city/:cityname", function (req, res) {
+  const place = req.body.cityname;
+  const appkey = "fdcb5b9745097d2dc601e7663dca0bed";
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=${appkey}`;
+
+  https.get(weatherUrl, function (response) {
+    let weatherData = "";
+    response.on("data", function (data) {
+      weatherData += data;
+    });
+
+    response.on("end", function () {
+      weatherData = JSON.parse(weatherData);
+      console.log(weatherData);
+
+      const name = weatherData.name;
+      const main = weatherData.weather[0].main;
+      const description = weatherData.weather[0].description;
+      const temp = weatherData.main.temp;
+      const feelsLike = weatherData.main.feels_like;
+      const tempMin = weatherData.main.temp_min;
+      const tempMax = weatherData.main.temp_max;
+      const pressure = weatherData.main.pressure;
+      const humidity = weatherData.main.humidity;
+      const windSpeed = weatherData.wind.speed;
+      const icon = weatherData.weather[0].icon;
+      const imageicon = `https://openweathermap.org/img/wn/${icon}.png`;
+
+      const resultHtml = `
+        <h2>Weather in ${name}</h2>
+        <p>Main Weather: ${main}</p>
+        <p>Description: ${description}</p>
+        <p>Temperature: ${temp} °C</p>
+        <p>Feels Like: ${feelsLike} °C</p>
+        <p>Min Temperature: ${tempMin} °C</p>
+        <p>Max Temperature: ${tempMax} °C</p>
+        <p>Pressure: ${pressure} hPa</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
+        <img src="${imageicon}" alt="Weather Icon">
+      `;
+      res.send(resultHtml);
+    });
+  });
+});
+
+app.get("/weather/:latitude/:longitude", function (req, res) {
+  const latitude = req.params.latitude;
+  const longitude = req.params.longitude;
+  const appkey = "fdcb5b9745097d2dc601e7663dca0bed";
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${appkey}`;
+
+  https.get(weatherUrl, function (response) {
+    let weatherData = "";
+    response.on("data", function (data) {
+      weatherData += data;
+    });
+
+    response.on("end", function () {
+      weatherData = JSON.parse(weatherData);
+      console.log(weatherData);
+
+      const name = weatherData.name;
+      const main = weatherData.weather[0].main;
+      const description = weatherData.weather[0].description;
+      const temp = weatherData.main.temp;
+      const feelsLike = weatherData.main.feels_like;
+      const tempMin = weatherData.main.temp_min;
+      const tempMax = weatherData.main.temp_max;
+      const humidity = weatherData.main.humidity;
+      const icon = weatherData.weather[0].icon;
+      const imageicon = `https://openweathermap.org/img/wn/${icon}.png`;
+
+      const resultHtml = `
+        <h2>Weather in ${name}</h2>
+        <p>Main Weather: ${main}</p>
+        <p>Description: ${description}</p>
+        <p>Temperature: ${temp} °C</p>
+        <p>Feels Like: ${feelsLike} °C</p>
+        <p>Min Temperature: ${tempMin} °C</p>
+        <p>Max Temperature: ${tempMax} °C</p>
+        <p>Humidity: ${humidity}%</p>
+        <img src="${imageicon}" alt="Weather Icon">
+      `;
+      res.send(resultHtml);
+    });
+  });
+});
+
+app.listen(9700, function () {
+  console.log("Server is running on port 9700");
+});
